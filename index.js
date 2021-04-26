631b90c973aece0d24ba65af5531971d549329ed
Myles Starr <mylesstarr@gmail.com>
2021-04-26 19:00:46 -0400

    commit tree d1c4162e5de88e191cd2c2e84cff621ef63436b1

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
-// Write your code here
+const num1 = 8
+const num2 = 7.75
+const num3 = 5
+const num4 = 19
+const set = (1,2,3,4,20)
+
+let multiply = (num1*num2) 
+
+let random = Number.parseInt((Math.random() +1), 10)
+
+let mod = (num4 % num3)
+    
+let max = Math.max(set);
+
+
